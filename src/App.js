c080bb71298abe5be24417cbb61a127dce0d7a38
asho221 <hello@flydry.co.uk>
2026-03-16 14:28:37 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SubscriptionBanner from './SubscriptionBanner';
+import Commercials from './Commercials';
 
 export default function App() {
-  return <SubscriptionBanner />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SubscriptionBanner />} />
+        <Route path="/commercials" element={<Commercials />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
